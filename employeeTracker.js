185d5f3c3b63457bcd71ee3cb850e2dcfddb44ed
javayporter <javayporter@gmail.com>
2020-07-17 12:11:14 -0400

    added functions to handle user selection

--- employeeTracker.js
+++ employeeTracker.js
@@ -30,7 +30,9 @@ function start() {
     "ADD ROLE",
     "VIEW ROLE",
     "ADD EMPLOYEE",
-    "VIEW EMPLOYEE"
+    "VIEW EMPLOYEE",
+    "UPDATE ROLE",
+    "END"
   ]
 })
     .then(function(answer) {
@@ -58,11 +60,104 @@ function start() {
       }
     });
 
-    //function to handle viewing departments
+    // function to handle adding a role
+    function addRole() {
+      inquirer.prompt([
+          {
+              type: "input",
+              name: "title",
+              message: "Enter Title: ",
+          }, {
+              type: "number",
+              name: "salary",
+              message: "Enter Salary: ",
+          }, {
+              type: "number",
+              name: "departmentId",
+              message: "Enter Depaertment ID: ",
+          }
+      ]).then(function (response) {
+          connection.query("INSERT INTO roles (title, salary, department_id) values (?, ?, ?)", [response.title, response.salary, response.departmentId], function (err, results) {
+              console.table(results);
+          })
+          start();
+      })
+  
+  }
+
+    // function to handle adding an employee 
+    function addEmployee() {
+      inquirer.prompt([{
+              type: "input",
+              name: "firstName",
+              message: "Enter employee first name. "
+          },
+          {
+              type: "input",
+              name: "lastName",
+              message: "Enter employee last name. "
+          },
+          {
+              type: "number",
+              name: "roleId",
+              message: "Enter role ID. "
+          },
+          {
+              type: "number",
+              name: "managerId",
+              message: "Enter manager ID. "
+          }
+      ]).then(function(res) {
+          connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.firstName, res.lastName, res.roleId, res.managerId], function(err, results) {
+              if (err) throw err;
+              console.table(results);
+              start();
+          })
+      })
+  } 
+    
+    // function to handle adding a department
+    function addDepartment() {
+      inquirer.prompt([{
+          type: "input",
+          name: "department",
+          message: "Enter Department Name: "
+      }, ]).then(function(res) {
+          connection.query('INSERT INTO department (name) VALUES (?)', [res.department], function(err, results) {
+              if (err) throw err;
+              console.table(results);
+              start();
+          })
+      })
+  }
+
+    // function to handle viewing roles
+    function viewRole() {
+      connection.query("SELECT * FROM role", function(err, results) {
+        if (err) throw (err);
+        console.table(results);
+        start();
+      });
+    }   
+    
+    // function to handle viewing employees
+    function viewEmployee() {
+      connection.query("SELECT * FROM employees", function(err, results) {
+        if (err) throw err;
+        console.table(results);
+        start();
+      })
+    } 
+
+    // function to handle viewing departments
     function viewDepartment() {
       connection.query("SELECT * FROM departments", function(err, results) {
         if (err) throw err;
         console.table(results);
+        start();
       });
+
+
+      
 }};
     
